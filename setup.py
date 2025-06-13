7c0b0af4fceaf755e84819c653500a14f9149887
Grant-Giesbrecht <giesbrechtgrant@gmail.com>
2025-06-13 10:01:57 -0600

    setup.py: Fixed fualty identation

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Operating System :: OS Independent",
     ],
-        install_requires=[
+    install_requires=[
         'Unidecode >= 1.2.0',
         'pandas >= 1.1.3',
         'setuptools >= 50.3.0',
